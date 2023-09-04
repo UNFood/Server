db29815fcf29c0b993760e9d4361b2f41bbaee91
Juan Bello <jdbelloro@unal.edu.co>
2023-09-03 21:28:11 -0500

    API functional

--- src/routes/server.ts
+++ src/routes/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import http from 'http';
 import mongoose from 'mongoose';
 import { config } from '../config/config';
-import Logging from '../library/logging';
+import exp from 'constants';
 
 const router = express();
 
@@ -11,7 +11,52 @@ mongoose
     .connect(config.mongo.url, {retryWrites: true, w: 'majority'})
     .then(() => {
         console.log('connected to mongoDB.');
+        StartServer();
     })
     .catch((error) => {
         console.log('Unable to connect');
     });
+
+//the server starts only if mongo connect
+const StartServer = () => {
+    router.use((req, res, next) => {
+        //log the request
+        console.log(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+        res.on('finish', () => {
+            //log the response
+            console.log(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${req.statusCode}]`);
+        })
+        next();
+    });
+
+    router.use(express.urlencoded({ extended: true }));
+    router.use(express.json());
+
+    //Rules for API
+    router.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*'); //Guess that in prod its only going to have the ips from the team and the frontend
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+        if (req.method == 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
+        next();
+    })
+
+    //Routes
+
+    //Healthcheck
+    router.get('/ping', (req, res, next) => res.status(200).json({ message: 'pong'}));
+
+    //Error handling
+    router.use((req, res, next) => {
+        const error = new Error('Not found');
+        console.log(error);
+
+        return res.status(404).json({ message: error.message});
+    });
+
+    http.createServer(router).listen(config.server.port, () => console.log(`Server is running on port: ${config.server.port}.`));
+};
