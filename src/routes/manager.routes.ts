eff0cb725974c659de37cb321ad4d61769878e5d
jhsilvaz13 <jhsilvaz@unal.edu.co>
2023-09-21 23:33:12 -0500

    Fix routes manager version

--- src/routes/manager.routes.ts
+++ src/routes/manager.routes.ts
@@ -5,6 +5,6 @@ import router from './product.routes';
 const router_manager = Router();
 
 //Add routes to api
-router_manager.use('/product', router);
+router_manager.use('/v1/product', router);
 
 export default router_manager;
