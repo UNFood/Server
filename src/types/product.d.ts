a5518b9b2a49eb64f9a2d0035d8d09a7c8226e8a
jhsilvaz13 <jhsilvaz@unal.edu.co>
2023-09-27 22:34:39 -0500

    Create Product Create Interface

--- src/types/product.d.ts
+++ src/types/product.d.ts
@@ -9,3 +9,13 @@ export interface ProductI {
   stock: Number | undefined;
   total_sales: Number;
 }
+
+export interface ProductCreateI {
+  chaza_name: String;
+  name: String;
+  description: String;
+  price: Number;
+  image: String;
+  category: Number;
+  stock: Number | undefined;
+}
