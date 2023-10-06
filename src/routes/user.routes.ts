eaac014d7402f695e0555970d9fcea8a12335c48
jubuenor <jubuenor@unal.edu.co>
2023-10-06 13:31:38 -0500

    user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,12 @@
 // userRoutes.ts
 import { Router } from "express";
-import UserService from "../services/user.service";
+import user from "../controllers/user.controller";
 
 const user_router = Router();
 
-user_router.get("/:id", UserService.get);
-user_router.get("/users", UserService.getAll);
-user_router.put("/", UserService.update);
-user_router.delete("/:id", UserService.delete);
+user_router.get("/:id", user.get);
+user_router.get("/users", user.getAll);
+user_router.put("/", user.update);
+user_router.delete("/:id", user.delete);
 
 export default user_router;
