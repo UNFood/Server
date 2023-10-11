dbe451be5a7b7105d90012c9116edb22a0dbda76
Juan Andres Bueno <104238278+jubuenor@users.noreply.github.com>
2023-10-10 21:40:07 -0500

    Update app.ts

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import { Router } from "express";
 import router_manager from "../routes/manager.routes";
-import fileUpload from "express-fileupload";
 
 // import routes...
 
@@ -13,14 +12,6 @@ app.use(express.static("public"));
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(express.json({ limit: "50mb" }));
-//app.use(
-//fileUpload({
-//useTempFiles: true,
-//safeFileNames: true,
-//preserveExtension: true,
-//tempFileDir: "/tmp/",
-//})
-//);
 
 app.get("/", (req, res) => {
   res.send("api version 1 en desarrollo ...");
