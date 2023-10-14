aae112e7d8e71a6c2617b4bc913e8b95cdffa872
jubuenor <jubuenor@unal.edu.co>
2023-10-14 18:34:37 -0500

    middleware chaza

--- src/routes/chaza.routes.ts
+++ src/routes/chaza.routes.ts
@@ -2,11 +2,12 @@
 import { Router } from "express";
 import chaza from "../controllers/chaza.controller";
 import chazaService from "../services/chaza.service";
+import { verifyTokenChaza } from "../middlewares/verify";
 
 const chaza_router = Router();
 
 //Routes
-chaza_router.get("/:id", chaza.getChaza);
+chaza_router.get("/byId/:id", chaza.getChaza);
 chaza_router.get("/byName/:name", chaza.getChazaByName);
 chaza_router.get("/chazas", chaza.getAllChazas);
 chaza_router.post(
@@ -14,7 +15,7 @@ chaza_router.post(
   chazaService.uploadImage.single("image"),
   chaza.createChaza
 );
-chaza_router.put("/", chaza.updateChaza);
-chaza_router.delete("/:id", chaza.deleteChaza);
+chaza_router.put("/", verifyTokenChaza, chaza.updateChaza);
+chaza_router.delete("/:id", verifyTokenChaza, chaza.deleteChaza);
 
 export default chaza_router;
